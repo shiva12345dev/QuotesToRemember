2a104a5411fb11b1634af3757f7fc6b4ccb98f7a
shiva12345dev <122954104+shiva12345dev@users.noreply.github.com>
2024-03-24 14:44:40 -0400

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,43 @@
+
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="challa",
+    password="MyDBPassword",
+    hostname="challa.mysql.pythonanywhere-services.com",
+    databasename="challa$quotes",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+    author = db.Column(db.String(512))
+
+
+#comments = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    #comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route("/hello")
+def hello():
+    return "<h2>Hello world!, from Shiva!</h2>"
